fd9c1579145e6a71dd61729b21f2d9e6171990f0
ryanmanny <ryan.manny@wsu.edu>
2019-03-28 14:30:53 -0700

    Add PICKUP_VERBS for now

--- lester/constants.py
+++ lester/constants.py
@@ -4,3 +4,4 @@ __VERB = SynonymFinder.VERB
 
 GO_VERBS = SynonymFinder('go', part_of_speech=__VERB).get()
 LOOK_VERBS = SynonymFinder('look', part_of_speech=__VERB).get()
+PICKUP_VERBS = SynonymFinder('grab', part_of_speech=__VERB).get()
