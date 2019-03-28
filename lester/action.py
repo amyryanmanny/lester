b71f8ca097469e2ddb65583d5766520c3916d39d
ryanmanny <ryan.manny@wsu.edu>
2019-03-28 11:19:41 -0700

    Define Action more rigidly

--- lester/action.py
+++ lester/action.py
@@ -1,13 +1,34 @@
+import lester.context
+
+
 class ActionMeta(type):
-    """
-    This doesn't really do anything, but maybe someday
-    """
     def __new__(mcs, name, bases, attrs):
         if not bases:  # To create an Action superclass with this Metaclass
             return super().__new__(mcs, name, bases, attrs)
+
+        if 'verbs' not in attrs:
+            raise AttributeError(f"{name} must have a list of verbs")
+        if getattr(attrs['verbs'], '__iter__') is None:
+            raise AttributeError(f"{name}'s verbs must be iterable")
+
+        if 'do' not in attrs:
+            raise AttributeError(f"{name} must have a do method defined")
+        elif not callable(attrs['do']):
+            raise AttributeError(f"{name}'s do method is not callable")
+
+        # Astonishes user by not forcing them to pass in self
+        # TODO: Figure out if it can just replace self with context instead
+        attrs['do'] = staticmethod(attrs['do'])
 
         return super().__new__(mcs, name, bases, attrs)
 
 
 class Action(metaclass=ActionMeta):
     verbs = []
+
+    @staticmethod
+    def do(context: lester.context.Context):
+        """
+        This base function exists to improve linting
+        """
+        raise NotImplementedError
