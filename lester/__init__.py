39875860acf8db0ca1635b8635ada3320e8f7d7b
ryanmanny <ryan.manny@wsu.edu>
2019-03-28 11:31:13 -0700

    Add top-level module exports

--- lester/__init__.py
+++ lester/__init__.py
@@ -1,9 +1,24 @@
 """
 Create setup.py for PyPi listing
 """
+from .app import App
+from .room import Room
+from .item import Item
+from .action import Action
 
-import nltk
 
-# TODO: Check if these can be skipped automatically if they already exist
-nltk.download('wordnet')
-nltk.download('brown')
+def lester_init(download_check=False):
+    if download_check:  # Make sure nltk is up to date
+        __download_nltk()
+
+
+# Export all Classes that a user will need to use Lester
+__all__ = ['App', 'Room', 'Item', 'Action', 'lester_init']
+
+
+def __download_nltk():
+    import nltk as __nltk
+
+    # TODO: Check if these can be skipped automatically if they already exist
+    __nltk.download('wordnet')
+    __nltk.download('brown')
